d3d565e6d693415df323b0907d848661207edf3f
Panda <mendeltem@googlemail.com>
2018-09-27 11:55:31 +0200

    update

--- saccade_lenght.R
+++ saccade_lenght.R
@@ -1,19 +1,21 @@
-setwd("E:/CS/Project Mathe/PointProcesses")
+setwd("~/Dokumente/Pychologie/sackade_lenght_distrubution")
 rm(list=ls())
 
-library(nlme)
-library(spatstat.data)
-library(rpart)
+#library(nlme)
+#library(spatstat.data)
+#library(rpart)
 library(dplyr)
-library(mgcv)
-library(spatstat)
+#library(mgcv)
+#library(spatstat)
 library(ggplot2)
 library(grid)
 library(jpeg)
 library(stringr)
 library(Hmisc)
 
-source("multiplot.R")
+source("multiplot.r")
+ls(pattern="*.dat")
+
 
 # load all the image experiment in one data
 filelist <- list.files(path = "data/",pattern = "fixloc", full.names=T)
@@ -36,16 +38,18 @@ all.df = all.df %>% transform(images = as.numeric(images),
 
 all.df =all.df %>% filter(!is.na(images) | !is.na(xpos) | !is.na(ypos) | !is.na(fixation))
 
-t= 1;dis = c();l= c();o = c();k=c();z=1;n =2;dis_x= c();dis_y= c(); time = 0; dist =c()
+
+
+a= c();b = c();c=c();z=1;n =2;dis_x= c();dis_y= c(); time = 0; dist =c()
 time_t = c()
 #1raw
 for(i in 1:nrow(all.df )){
 
   if(all.df[i,"trial"] == z){
     p= n-1
-    l = abs(abs(all.df[n, "xpos"]) - abs(all.df[p, "xpos"]))
-    o = abs(abs(all.df[n, "ypos"]) - abs(all.df[p, "ypos"]))
-    k = sqrt(l^2 + o^2)
+    a = abs(abs(all.df[n, "xpos"]) - abs(all.df[p, "xpos"]))
+    b = abs(abs(all.df[n, "ypos"]) - abs(all.df[p, "ypos"]))
+    c = sqrt(a^2 + b^2)
     time = time + all.df[p, "fdur"]
     n = n +1     
   } else{
@@ -59,9 +63,9 @@ for(i in 1:nrow(all.df )){
     dist <- c(dist, 0)
     time = all.df[i,"fdur"]
   }
-  dis_x <-c(dis_x,  l)
-  dis_y <-c(dis_y,  o)
-  dist  <-c(dist, k)
+  dis_x <-c(dis_x,  a)
+  dis_y <-c(dis_y,  b)
+  dist  <-c(dist, c)
   
   time_t <- c(time_t, time)
   
@@ -112,14 +116,36 @@ p3 = ggplot(df_fixdur, aes(rownames(df_fixdur), mean_fixdur))+
   ylab("Mean Fixation Duration")+
   ggtitle("Mean Fixation Duration Distance for each Subject", subtitle = "")
 p2 = ggplot(each_image, aes(rownames(each_image),mean_distance))+
+  geom_point(stat="identity",na.rm = TRUE)+
   scale_x_discrete((limits=rownames(each_image)))+
   ylab("Mean Sackadlenght")+
   xlab("Images")+
   ggtitle("Sackadlength for each Image")
 
-multiplot(p1, p3, cols=2)
+
+
+h1=ggplot(df_mean_perfixation_subj, aes(df_mean_perfixation_subj$mean_distance)) + 
+  geom_histogram(breaks =seq(4,9,by=0.4),
+                 col="blue",
+                 fill = "red",
+                 alpha  = .5) +
+  labs(title = "mean Saccadelength for each Subject", x="Sackade Length", y = "Count")
 
 
 
 
+h2 =ggplot(each_image, aes(each_image$mean_distance)) + 
+  geom_histogram(breaks =seq(5.5,6.7,by=0.15),
+                 col="blue",
+                 fill = "red",
+                 alpha  = .5) +
+  labs(title = "mean Saccadelength for each Image", x="Sackade Length", y = "Count")
 
+
+
+multiplot(p1, p3, p2,h1,h2, cols=3)
+
+
+#ggplot(df_mean_perfixation_subj,aes(mean_distance, mean_fixdur), color=subj) + 
+#  geom_point()+
+#  geom_abline(intercept = 1, slope = 1.3)
